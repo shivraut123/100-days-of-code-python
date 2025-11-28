85d2f487a46224a8420222e541fe088feab182d7
shivraut123 <shivraut361@gmail.com>
2025-11-28 22:39:35 +0545

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator Project")
+city=input("What city did you grow up ")
+pet=input("What is the name of your pet ")
+print( "Your band could be: " + city + " " + pet)
+
