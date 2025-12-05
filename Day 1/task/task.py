e94fe02c240da8c57e46fed43d61c1426ae2e6b5
shivraut123 <shivraut361@gmail.com>
2025-12-05 10:44:00 +0545

    Day 1: Band Name Generator

--- Day 1/task/task.py
+++
@@ -1,7 +0,0 @@
-# This is the input function
-# print("Hello " + input("What is your Name ? "))
-# This below code is username is variable which takes input and gives it to print funcn
-username= input("What is Your Name? ")  
-# print("Hello " + username)
-print("Hello" + " " + username + "!")
-
