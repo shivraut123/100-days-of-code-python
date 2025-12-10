e7c469ff2749fa87f6c5a23f0484167f292c4461
shivraut123 <shivraut361@gmail.com>
2025-12-10 16:09:19 +0545

    Add .gitignore

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,7 @@
-print("Welcome to the Band Name Generator Project")
-city=input("What city did you grow up ")
-pet=input("What is the name of your pet ")
-print( "Your band could be: " + city + " " + pet)
+# This is the input function
+# print("Hello " + input("What is your Name ? "))
+# This below code is username is variable which takes input and gives it to print funcn
+username= input("What is Your Name? ")  
+# print("Hello " + username)
+print("Hello" + " " + username + "!")
 
---
+++ Day 6/Functions/task.py
@@ -0,0 +1,13 @@
+# def info():
+#     name = input("What is your Name? ")
+#     print("Hello, " + name)
+#
+# print("Hello")
+# info()
+
+
+no_of_hurdles = 6
+while no_of_hurdles > 0:
+    print("Hello")
+    no_of_hurdles -= 1
+
