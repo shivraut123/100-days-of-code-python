e6e5469ae17df3823cb6b2227e5d51d05aa5da35
shivraut123 <shivraut361@gmail.com>
2025-12-12 22:47:30 +0545

    Day 9: Blind Auction Project

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,53 @@
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+
+import art
+
+print(art.logo)
+
+bids = {}
+
+
+def find_highest_bidder(bidder_dictionary):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidder_dictionary:
+        bid_amount = bidder_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}!")
+
+
+continue_bidding = True
+
+while continue_bidding:
+    name = input("What is your name? ")
+    price = int(input("What is your bidding amount? $"))
+
+    # Store bid in dictionary
+    bids[name] = price
+
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)  # Clears screen (simulated)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,26 @@
+capitals = {"France":"Paris",
+            "Germany":"Berlin"
+}
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+}
+
+print(travel_log["France"][1])
+
+# Nested lists
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1])
+
+#Nesting a Dictionary inside a dictionary
+travel_log = {"France": {
+    "cities_visited": ["Paris","Lille","Dijon"],
+    "total_visits": 12
+},
+"Germany": {
+    "cities_visited": ["Berlin","Hamburg","Stuttgart"],
+    "total_visits": 5
+},
+
+}
+print(travel_log["Germany"]["cities_visited"][2])
