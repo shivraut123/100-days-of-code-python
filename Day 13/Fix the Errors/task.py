af534951afd2f40eaa205ed795d841a19ca768f7
shivraut123 <shivraut361@gmail.com>
2025-12-16 17:18:47 +0545

    Day 13: Debugging

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,10 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have entered invalid no. Enter numerical value like 20,17")
+    age = int(input("How old are you ?"))
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
+else:
+    print(f"You are not eligible to drive at {age}")
+
