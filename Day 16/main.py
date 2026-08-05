e08879ce1f3616d1aae01448a5783024f94d4765
shivraut123 <shivraut361@gmail.com>
2026-08-05 10:44:01 +0545

    Updated README file

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,17 +1,9 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+p1 = Person("Shiv Kumar Raut", 25)
+print(p1.name)
+print(p1.age)
