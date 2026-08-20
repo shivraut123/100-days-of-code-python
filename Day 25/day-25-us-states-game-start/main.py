0d977075ec2a7f8f79ff1a9f26fc84ee12dceacf
shivraut123 <shivraut361@gmail.com>
2026-08-20 16:34:41 +0545

    updating code

--- Day 25/day-25-us-states-game-start/main.py
+++ Day 25/day-25-us-states-game-start/main.py
@@ -15,11 +15,12 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="Enter another state name:").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-        print(missing_states)
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        # print(missing_states)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
